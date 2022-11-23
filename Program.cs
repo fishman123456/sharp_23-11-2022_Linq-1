1be42da6e3b18f93dca5ee2d1f44ea00acb34299
F_D <den9152@yandex.ru>
2022-11-23 20:48:00 +0700

    1
    1

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ foreach (var a in arr )
 }
 Console.WriteLine("---------------------");
 var red = from r in arr
-          where r % 3 == 0 && r %5!= 0
+          where r % 3 != 0 && r %5!= 0
           select r;
 
 foreach (var r in red)
