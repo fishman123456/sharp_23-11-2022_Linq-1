73cf57a0e425d2bc31582850c2f2172363ac82bd
F_D <den9152@yandex.ru>
2022-11-23 20:41:00 +0700

    готово

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿
+Random rand = new Random();
+int [] arr = new int[10];    
+
+for (int i = 0; i < 10; i++)
+{
+    arr[i] = rand.Next(1,100);
+}
+foreach (var a in arr )
+{
+    Console.WriteLine(a);
+}
+Console.WriteLine("---------------------");
+var red = from r in arr
+          where r % 3 == 0 && r %5!= 0
+          select r;
+
+foreach (var r in red)
+{
+    Console.WriteLine(r);
+}
